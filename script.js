921ad732a38198d4dd6c55e06cb7d32d071af6e5
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-13 16:25:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+  let sampleTable=document.getElementById("sampleTable");
+	let tr=document.createElement("tr");
+	let td1=document.createElement("td");
+	let td2=document.createElement("td");
+	td1.textContent="New Cell1";
+	td2.textContent="New Cell2";
+	
+	tr.append(td1,td2);
+	sampleTable.insertBefore(tr, sampleTable.firstChild);
   
 }
