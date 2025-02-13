47b7df84344e1668359cd6ce400ac5206f0de87d
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-13 16:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	let tr=document.createElement("tr");
 	let td1=document.createElement("td");
 	let td2=document.createElement("td");
-	td1.textContent="New  Cell1";
-	td2.textContent="New  Cell2";
+	td1.innerText="New  Cell1";
+	td2.innerText="New  Cell2";
 	
 	tr.append(td1,td2);
 	// sampleTable.insertBefore(tr, sampleTable.firstChild);
