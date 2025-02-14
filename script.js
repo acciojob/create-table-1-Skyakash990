1fe029a296d2245f93020a235b4fd8d2f8a3d21d
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-14 11:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function insert_Row() {
 	let tr=document.createElement("tr");
 	let td1=document.createElement("td");
 	let td2=document.createElement("td");
-	td1.textContent="New Cell1";
-	td2.textContent="New Cell2";
+	td1.innerText="New Cell1";
+	td2.innerText="New Cell2";
 	
 	tr.append(td1,td2);
-	// sampleTable.tBodies[0].insertBefore(tr, sampleTable.rows[0]);
+	sampleTable.tBodies[0].insertBefore(tr, sampleTable.rows[0]);
   
 }
